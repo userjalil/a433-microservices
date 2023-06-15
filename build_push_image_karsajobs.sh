cd4fabfdfbbadfa6acf0ea355b8c9e6d572061a4
user-jalil <config.sanasini@gmail.com>
2023-06-15 22:33:50 +0800

    add build_push_image script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+GIT_USER="userjalil"
+
+docker build -t ghcr.io/$GIT_USER/karsajobs:latest .
+
+echo "$GIT_PASS" | ghcr login ghcr.io -u "$GIT_USER" --password-stdin
+
+docker push ghcr.io/$GIT_USER/karsajobs:latest
+
