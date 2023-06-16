500706aa79069637c10c375c09027b9d1584e024
userjalil <115966467+userjalil@users.noreply.github.com>
2023-06-17 03:28:39 +0800

    Update build_push_image_karsajobs-ui.sh

--- build_push_image_karsajobs-ui.sh
+++ build_push_image_karsajobs-ui.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# set user github
 GIT_USER="userjalil"
 
+# build image karsajob-ui
 docker build -t ghcr.io/$GIT_USER/karsajobs-ui:latest .
 
+# login ke github registry
+# password/token github sudah di export sebelum shell script di jalankan menggunakan variabel GIT_PASS
 echo "$GIT_PASS" | ghcr login ghcr.io -u "$GIT_USER" --password-stdin
 
+# push image karsajob-ui ke github registry
 docker push ghcr.io/$GIT_USER/karsajobs-ui:latest
 
