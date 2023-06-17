0a68095db609dcb1ceadb2e1873d7a7e8aad622d
user-jalil <config.sanasini@gmail.com>
2023-06-17 17:57:12 +0800

    update CI pipeline karsajobs

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -8,7 +8,7 @@ docker build -t ghcr.io/$GIT_USER/karsajobs:latest .
 
 # login ke github registry
 # password/token github sudah di export sebelumnya dengan variabel GIT_PASS
-echo "$GIT_PASS" | ghcr login ghcr.io -u "$GIT_USER" --password-stdin
+echo "$GIT_PASS" | docker login ghcr.io -u "$GIT_USER" --password-stdin
 
 # push image karsajobs ke github registry
 docker push ghcr.io/$GIT_USER/karsajobs:latest
