dc427ec30f29016f2ddbda28b8907fad6fee3cc3
user-jalil <config.sanasini@gmail.com>
2023-06-17 19:38:28 +0800

    update build push image

--- build_push_image_karsajobs-ui.sh
+++ build_push_image_karsajobs-ui.sh
@@ -8,7 +8,7 @@ docker build -t ghcr.io/$GIT_USER/karsajobs-ui:latest .
 
 # login ke github registry
 # password/token github sudah di export sebelum shell script di jalankan menggunakan variabel GIT_PASS
-echo "$GIT_PASS" | ghcr login ghcr.io -u "$GIT_USER" --password-stdin
+echo "$GIT_PASS" | dockerr login ghcr.io -u "$GIT_USER" --password-stdin
 
 # push image karsajob-ui ke github registry
 docker push ghcr.io/$GIT_USER/karsajobs-ui:latest
