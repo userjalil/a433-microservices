a46caf551e12cee61e835ee1c5a6343cbcdbdc07
userjalil <config.sanasini@gmail.com>
2023-06-28 15:45:10 +0800

    add script build push image

--- build_push_image_shipping-app.sh
+++ build_push_image_shipping-app.sh
@@ -3,12 +3,12 @@
 # set user github
 GIT_USER="userjalil"
 
-# build image order-app
-docker build -t ghcr.io/$GIT_USER/order-app:latest .
+# build image shipping-app
+docker build -t ghcr.io/$GIT_USER/shipping-app:latest .
 
 # login ke github registry
 # password/token github sudah di export sebelumnya dengan variabel GIT_PASS
 echo "$GIT_PASS" | docker login ghcr.io -u "$GIT_USER" --password-stdin
 
-# push image order-app ke github registry
-docker push ghcr.io/$GIT_USER/order-app:latest
+# push image shipping-app ke github registry
+docker push ghcr.io/$GIT_USER/shipping-app:latest
